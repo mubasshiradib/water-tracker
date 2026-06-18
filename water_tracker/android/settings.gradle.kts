41a827229310b12d5044b5bcd5f43b9508ac77e5
Mubasshir Adib <mubasshiradib@gmail.com>
2026-06-19 01:36:33 +0600

    build: initialize Android Gradle project structure with standard build properties and plugin management

--- water_tracker/android/settings.gradle.kts
+++ water_tracker/android/settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.2.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
